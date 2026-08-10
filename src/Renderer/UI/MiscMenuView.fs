46090a51e59ca49037f1e539b665b2cfafa4d9e8
Lucas <98566376+LWZsama@users.noreply.github.com>
2026-08-10 12:57:02 +0800

    fix(browser): use upstream logging API

--- src/Renderer/UI/MiscMenuView.fs
+++ src/Renderer/UI/MiscMenuView.fs
@@ -1000,5 +1000,5 @@ let importSheet model dispatch =
                             dispatch <| SetProject p
                         | _ -> ()
                         importSheetPopup projectDir paths sourceProjectDir dispatch))
-            (fun err -> JSHelpers.log err.Message)
+            (fun err -> Log.error err.Message)
 
